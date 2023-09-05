8fc8219f5e7b01b4adb9d34b291c4935f0743a6b
Alvaro <alvarord519@gmail.com>
2023-09-04 23:52:36 -0500

    first commit

---
+++ twitter-card/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <section className='tw-mainCard'>
+      <header>
+        <h1>Who to follow</h1>
+      </header>
+
+      <main>
+
+      </main>
+
+      <footer className='tw-cardFooter'>
+        <h3>Show more</h3>
+      </footer>
+
+    </section>
+  )
+}
+
+export default App

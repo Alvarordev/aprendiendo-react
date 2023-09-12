d566f11712efd85ed865d0ff8290714c9b871098
Alvaro <alvarord519@gmail.com>
2023-09-12 14:45:35 -0500

    Fetching the data from the APIs

--- 04-react-prueba-tecnica/README.md
+++ 04-react-prueba-tecnica/README.md
@@ -2,6 +2,6 @@
 
 APIs:
 - Facts Random: https://catfact.ninja/fact
-- Imagen Random: https://cataas.com
+- Imagen Random: `https://cataas.com/cat/says/${fact}?json=true`
 
 Recupera un hecho aleatorio de gatos de la primera API y muestra una imagen de un gato con la palabra del hecho usando la segunda API
--- 04-react-prueba-tecnica/src/App.jsx
+++ 04-react-prueba-tecnica/src/App.jsx
@@ -1,11 +1,41 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import './App.css'
+
+const CAT_FACT_URL = 'https://catfact.ninja/fact'
 
 const App = () => {
-  const [catFact, setCatFact] = useState()
+  const [fact, setFact] = useState()
+  const [imageUrl, setImageUrl] = useState()
+
+  useEffect(() => {
+    fetch(CAT_FACT_URL)
+      .then((res) =>
+        // TODO: Handle error
+        res.json())
+      .then(response => {
+        const { fact } = response
+        setFact(fact)
+      })
+  }, [])
+
+  useEffect(() => {
+    if (!fact) return
+
+    const firstThreeWords = fact.split(' ', 3).join(' ')
+
+    fetch(`https://cataas.com/cat/says/${firstThreeWords}?size=50&color=red&json=true`)
+      .then((res) => res.json())
+      .then(response => {
+        const { url } = response
+        setImageUrl(url)
+      })
+  }, [fact])
 
   return (
     <main>
       <h1>Aplicacion de gatos</h1>
+      <p>{fact}</p>
+      {imageUrl && <img src={`https://cataas.com${imageUrl}`} alt='Random cat fact image' />}
     </main>
   )
 }
